dab5616e48d16f1a4ec309fbba919da5f2614318
Racxshan Nagalingam <510460@student.fontys.nl>
2023-04-13 15:40:17 +0200

    Versie waarvan het in meer functies opgesplits is.

--- main.go
+++ main.go
@@ -1,67 +1,68 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"os"
 )
 
+type Color struct {
+	Kleur   string `json:"name"`
+	Gedicht string `json:"text"`
+}
+
 func main() {
-	var kleur string
-	fmt.Println("Wat is je favoriete kleur? ")
-	fmt.Scanln(&kleur) //Hier wordt gescant wat je in de terminal typt. Wat je in de terminal typt wordt gezien als 'kleur'.
-	zin, fout := getSentence(kleur)
-	if fout != nil {
-		fmt.Println("Er is een fout opgetreden:", fout)
-	}
-
-	err := writeToFile(zin)
+	kleur := getInput("Wat is je favoriete kleur?")
+	tekst, err := getTekst(kleur)
 	if err != nil {
-		fmt.Println("couldn't write to file", err)
-		os.Exit(1)
+		handleError("Er is een fout opgetreden:", err, 1)
 	}
+	err = writeToFile("gedicht.txt", tekst)
+	if err != nil {
+		handleError("Kon de gedicht niet naar het bestand schrijven.", err, 2)
+	}
+	fmt.Println("Gedicht is geschreven naar gedicht.txt")
 }
 
-func getSentence(kleur string) (string, error) {
-	var tekst string //String voor de tekst wat in de textfile komt.
-	switch kleur {   //Een switch statement voor de verschillende mogelijkheden.
-	case "rood":
-		tekst = "Rood met passie."
-	case "blauw":
-		tekst = "Blauw zoals de lucht."
-	case "groen":
-		tekst = "Groen van de natuur."
-	case "geel":
-		tekst = "Geel als de stralen van de zon."
-	case "paars":
-		tekst = "Paars als de nacht."
-	case "oranje":
-		tekst = "Oranje als de zonsondergang."
-	case "wit":
-		tekst = "Wit als de sneeuw."
-	case "roze":
-		tekst = "Roze als de bloesem."
-	default: //Dit gebeurt er als je iets intypt wat niet bij de case hoort.
-		//fmt.Println("Sorry, ik ken deze kleur niet.")
-		//os.Exit(1) //Foutmelding.
-		return "", fmt.Errorf("kleur %s is niet bekend", kleur) //%s is wat ik in de terminal invoer.
-	}
-	return tekst, nil
+func getInput(vraag string) string {
+	var inputkleur string
+	fmt.Println(vraag)
+	fmt.Scanln(&inputkleur)
+	return inputkleur
 }
 
-func writeToFile(sentence string) error {
-	file, fout := os.Create("gedicht.txt") //Hier wordt met behulp van os. een textfile aangemaakt. En  de woorden: "file" en "fout" wordt eraan gekoppeld, zodat je er later naar kan verwijzen voor bijvoorbeeld een actie.
-	if fout != nil {
-		return fout //Als fout niet niks is, oftewel als er fouten inzitten print het een tekst met een foutcode.
-		//fmt.Println("Kon het bestand niet maken.")
-		//os.Exit(1)
+func getTekst(kleur string) (string, error) {
+	file, err := ioutil.ReadFile("tekst.json")
+	if err != nil {
+		return "", err
 	}
-	defer file.Close() //Dit wordt automatisch aan het einde geplaatst als de code runt. Zodat de code altijd wordt afgesloten.
+	var kleuren []Color
+	err = json.Unmarshal([]byte(file), &kleuren)
 
-	_, fout = file.WriteString(sentence) //Hier wordt de text gewrite naar de file. De underscore is daar als een 'valse' waarde. Zodat het alleen focust op 'fout'.
-	if fout != nil {                     //Als fout niet niks is, oftewel als er fouten inzitten print het een tekst met een foutcode.
-		return fout
-		//fmt.Println("Kon de tekst niet naar het bestand schrijven.")
-		//os.Exit(1)
+	for _, color := range kleuren {
+		if color.Kleur == kleur {
+			return color.Gedicht, nil
+		}
+	}
+	return "", fmt.Errorf("Kleur is niet beschikbaar")
+}
+
+func writeToFile(filename string, inhoud string) error {
+	file, err := os.Create(filename)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	_, err = file.WriteString(inhoud)
+	if err != nil {
+		return err
 	}
 	return nil
+}
+
+func handleError(boodschap string, err error, exitcode int) {
+	fmt.Println(boodschap, err)
+	os.Exit(exitcode)
 }
